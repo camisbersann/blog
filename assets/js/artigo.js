4e387aec24a1d08cd152b22f3e39fc337f49e3c5
camilabersan <camilabersan.564@gmail.com>
2023-06-16 11:13:01 -0300

    commit2

--- assets/js/artigo.js
+++ assets/js/artigo.js
@@ -9,7 +9,6 @@ function publicar(){
     var entrada3 = document.getElementById("inp4").value;
     var entrada4 = document.getElementById("inp5").value;
 
-    entrada4 = entrada4.split("-").reverse('').join('/');
 
     noticia = [entrada, entrada1, entrada2, entrada3, entrada4];
 
@@ -31,16 +30,26 @@ function publicar(){
     console.log(conteudo);
 
     var contArr= "";
-    for(let i = 0; i< conteudo.length; i++){
+    let i = 0;
+    for( i = 0; i< conteudo.length; i++){
         var item = conteudo[i];
         contArr += `<p class = teste> Titulo: ${item[0]} </p> `;
         contArr += `<p class = teste> Resumo: ${item[1]} </p> `;
         contArr += `<p class = teste> Categoria: ${item[2]} </p> `;
         contArr += `<p class = teste> Autor: ${item[3]} </p> `;
-        contArr += `<p class = teste> Autor: ${item[4]} </p> `;
+        contArr += `<p class = teste> Data: ${item[4].split("-").reverse('').join('/')} </p> `;
+        contArr += `<button class= "button" id="bt" onclick="remover(${i})"> Remover </button>`;
+        contArr += `<button class= "button" id="bt" onclick="editar()"> Editar </button>`;
     
     }
 
     document.getElementById("result").innerHTML = contArr;
 }
 }
+
+
+function remover(i) {
+    conteudo.splice([i],1);
+    document.getElementsByClassName("teste").innerHTML = ""
+    console.log(conteudo)
+    }
