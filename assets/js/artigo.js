1c55d7fa632d755d7bb16506c3ecfcc5a49b0b66
camilabersan <camilabersan.564@gmail.com>
2023-06-16 14:59:25 -0300

    commit3

--- assets/js/artigo.js
+++ assets/js/artigo.js
@@ -2,54 +2,58 @@ var conteudo = [];
 var noticia = [];
 
 
-function publicar(){
-    var entrada = document.getElementById("inp1").value;
-    var entrada1 = document.getElementById("inp2").value;
-    var entrada2 = document.getElementById("inp3").value;
-    var entrada3 = document.getElementById("inp4").value;
-    var entrada4 = document.getElementById("inp5").value;
+function addItem() {
+    var titulo = document.getElementById("inp1").value;
+    var resumo = document.getElementById("inp2").value;
+    var categoria = document.getElementById("inp3").value;
+    var autor = document.getElementById("inp4").value;
+    var data = document.getElementById("inp5").value;
 
+    noticia = [titulo, resumo, categoria, autor, data];
 
-    noticia = [entrada, entrada1, entrada2, entrada3, entrada4];
-
-
-    if(entrada == ""|| entrada1 == ""|| entrada2 == ""|| entrada3 == ""|| entrada4 == ""){
+    if (titulo == "" || resumo == "" || categoria == "" || autor == "" || data == "") {
         console.log("entrou");
         document.getElementById("result1").innerHTML = "erro";
-        }
-    else{
+    }
+    else {
         document.getElementById("result1").innerHTML = "";
 
-    document.getElementById("inp1").value = "";
-    document.getElementById("inp2").value = "";
-    document.getElementById("inp3").value = "";
-    document.getElementById("inp4").value = "";
-    document.getElementById("inp5").value = "";
+        document.getElementById("inp1").value = "";
+        document.getElementById("inp2").value = "";
+        document.getElementById("inp3").value = "";
+        document.getElementById("inp4").value = "";
+        document.getElementById("inp5").value = "";
 
-    conteudo.push(noticia);
-    console.log(conteudo);
+        conteudo.push(noticia);
+        console.log(conteudo);
 
-    var contArr= "";
-    let i = 0;
-    for( i = 0; i< conteudo.length; i++){
+        atualizaTela();
+
+    }
+}
+
+function atualizaTela() {
+    var msg = "";
+    for (let i = 0; i < conteudo.length; i++) {
         var item = conteudo[i];
-        contArr += `<p class = teste> Titulo: ${item[0]} </p> `;
-        contArr += `<p class = teste> Resumo: ${item[1]} </p> `;
-        contArr += `<p class = teste> Categoria: ${item[2]} </p> `;
-        contArr += `<p class = teste> Autor: ${item[3]} </p> `;
-        contArr += `<p class = teste> Data: ${item[4].split("-").reverse('').join('/')} </p> `;
-        contArr += `<button class= "button" id="bt" onclick="remover(${i})"> Remover </button>`;
-        contArr += `<button class= "button" id="bt" onclick="editar()"> Editar </button>`;
-    
+        msg += `
+       <div class = "itemCard">
+       <div class = "internCard">
+       <p class = teste> Titulo: ${item[0]} </p> 
+       <p class = teste> Resumo: ${item[1]} </p>
+       <p class = teste> Categoria: ${item[2]} </p> 
+       <p class = teste> Autor: ${item[3]} </p>
+       <p class = teste> Data: ${item[4].split("-").reverse('').join('/')} </p> 
+       <button class= "button" id="bt" onclick="remover(${i})"> Remover </button>
+       </div>
+       </div>
+        `
     }
-
-    document.getElementById("result").innerHTML = contArr;
-}
+    document.getElementById("result").innerHTML = msg;
 }
 
 
-function remover(i) {
-    conteudo.splice([i],1);
-    document.getElementsByClassName("teste").innerHTML = ""
-    console.log(conteudo)
-    }
+function remover(index) {
+    conteudo.splice(index, 1);
+    atualizaTela();
+}
